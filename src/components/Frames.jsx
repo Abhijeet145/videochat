770d51e51fd33e990960284f03a731588fa3f437
Abhijeet Kumar <abhijeet145.akm@gmail.com>
2024-11-03 18:03:46 +0530

    Initial commit

---
+++ src/components/Frames.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Frames() {
+  return (
+    <div id="videos">
+      <video class="video-player" id="user-1" autoPlay playsInline></video>
+      <video class="video-player" id="user-2" autoPlay playsInline></video>
+    </div>
+  );
+}
+
+export default Frames;
+
+
---
+++ src/components/StreamHandler.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StreamHandler = () => {
+    let localStream=10;
+    let remoteStream;
+    return(
+        <div>Hello</div>
+    );
+}
+
+export default StreamHandler;
