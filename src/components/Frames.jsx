61bc1b5427db76c36be94ecfa49d9aa2f11f3c15
Abhijeet Kumar <abhijeet145.akm@gmail.com>
2024-11-17 02:49:52 +0530

    changes for multi user support

--- src/components/Frames.jsx
+++ src/components/Frames.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+//depending on number of frames they are rendered differently
 function Frames() {
   return (
     <div id="videos">
       <video className="video-player" id="user-1" autoPlay playsInline></video>
       <video className="video-player" id="user-2" autoPlay playsInline></video>
+      <video className="video-player" id="user-3" autoPlay playsInline></video>
+      <video className="video-player" id="user-4" autoPlay playsInline></video>
+      <video className="video-player" id="user-5" autoPlay playsInline></video>
     </div>
   );
 }
