5acbe9a1d9c7c0875ee2367de63eb2571e4f735c
Abhijeet Kumar <abhijeet145.akm@gmail.com>
2024-11-27 00:17:53 +0530

    fixing minor bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year} Abhijeet Kumar</p>
+      <p>Copyright ⓒ {year} NIT CSE</p>
     </footer>
   );
 }
